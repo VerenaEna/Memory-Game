a0c79afc9beacddc7747edce2c689c1c9454d1f1
Verena Ena <verenaparaluxe@gmail.com>
2018-04-03 18:58:53 +0200

    docs: add comments for better understanding

--- js/app.js
+++ js/app.js
@@ -1,36 +1,47 @@
-/**** All Variables ****/
-
+/*
+ **** All Variables ****
+*/
 // cards array holds all cards
 let card = document.getElementsByClassName('card');
 let cards = [...card];
 console.log(cards);
+
 // define move Variable
 let moves = 0;
 let counter = document.getElementsByClassName('moves');
+
 // define star icon Variable
 const stars = document.querySelectorAll('.fa-star');
+
 //define for timer function
 let second = 0;
 let minute = 0;
 let hour = 0;
 let timer = document.querySelector('.timer');
+
 //deck of all cards in game
 const deck = document.getElementById('card-deck');
+
 //variable of matchedCards
 let matchedCard = document.getElementsByClassName('match');
+
 //array opend cards
 let openedCards = [];
+
 // define modal Variable
 const modal = document.getElementById('popup1');
+
 //define stars list variable
 const starsList = document.querySelectorAll('.stars li');
+
 //define close icon
 const closeIcon = document.querySelector('.close');
 
 
-
-/**** All Functions ****/
-
+/*
+ **** All Functions ***
+*/
+// @description: shuffles cards (provided)
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -45,11 +56,11 @@ function shuffle(array) {
 
     return array;
 }
-//shuffles cards when site page is refreshed/load
+// @description: shuffles cards when site page is load/refreshed
 document.body.onload = startGame();
 
-//function start game will shuffle and display each card
-// click on restart button - reset star rating, move counting, deck visibility
+// @description: function start game will shuffle and display each card
+// @description: click on restart button - reset star rating, move counting, deck visibility
 function startGame(){
   // shuffle this deck
  cards = shuffle(cards);
@@ -74,14 +85,14 @@ function startGame(){
   timer.innerHTML = minute + ' min ' + second + ' sec';
   clearInterval(interval);
 }
-
+// @description: toggles classes to display cards
  var displayCard = function (){
    this.classList.toggle('open');
    this.classList.toggle('show');
    this.classList.toggle('disabled');
  }
 
-// add/push opened cards to OpenedCards array and
+// @description: add/push opened cards to OpenedCards array and
 function cardOpen(){
   openedCards.push(this);
   let len = openedCards.length;
@@ -105,18 +116,18 @@ function matched(){
   openedCards = [];
 }
 
-//if cards don't match add classList unmatched, set cards disable
+//if cards don't match set unmatched, set cards disable
 function unmatched(){
   openedCards[0].classList.add('unmatched');
   openedCards[1].classList.add('unmatched');
   disable();
-  // remove all classLists and enable cards and send new list
+  // remove all classLists and enable cards
   setTimeout(function(){
     openedCards[0].classList.remove('show', 'open', 'no-event', 'unmatched');
     openedCards[1].classList.remove('show', 'open', 'no-event', 'unmatched');
     enable();
     openedCards = [];
-  },1000);
+  }, 1000);
 }
 
 //disable temporarly cards
@@ -136,7 +147,7 @@ function enable(){
   });
 }
 
-// count player move and...
+// @description: count player move and...
 function moveCounter(){
   moves++;
   counter.innerHTML = moves;
@@ -165,7 +176,7 @@ function moveCounter(){
 }
 // define for startTimer
 var interval;
-// timer function
+// @description: timer function
 function startTimer(){
   interval = setInterval(function(){
     timer.innerHTML = minute + ' min ' + second + ' sec ';
@@ -180,20 +191,20 @@ function startTimer(){
     }
   }, 1000);
 }
-// winner modal if player found all pairs
+// @description: winner modal if player found all 16 pairs
 function winner(){
-  if(matchedCard.length == 16){ // if player found 16 pairs
+  if(matchedCard.length == 16){
     clearInterval(interval);
-    finalTime = timer.innerHTML; //show final Time
+    finalTime = timer.innerHTML; //show final Time on modal
     // add class to winner modal
     modal.classList.add('show');// to show on page
-    //define star rating Variable
+    //define star rating Variable 
     const starRating = document.querySelector('.stars').innerHTML;
-    //show move, rating and time on modal
+    //show move, star rating and time on modal
     document.getElementById('finalMove').innerHTML = moves;
     document.getElementById('starRating').innerHTML = starRating;
     document.getElementById('totalTime').innerHTML = finalTime;
-    //TODO: close icon on modal
+    //close icon
     closeModal();
   };
 }
